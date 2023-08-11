76db6eff07770b0f6ac1d9a3c14b8f4a655394af
Gaurav Kaushik <Gaurav.Kaushik@kelltontech.com>
2023-08-11 11:41:32 +0530

    ShoppingcartAPI basic setup in program.cs, ApplicationDBContext, Models, dtos, appSettings.json etc

---
+++ Mango.Services.ShoppingCart/Data/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Mango.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace Mango.Services.ShoppingCartAPI.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Mango.Services.ShoppingCart/Models/Dto/CartDetailsDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Mango.Services.ShoppingCartAPI.Models.Dtos;
+
+namespace Mango.Services.ShoppingCartAPI.Models.Dto
+{
+    public class CartDetailsDto
+    {
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        public CartHeader? CartHeader { get; set; }
+        public int ProductId { get; set; }
+        public ProductDto? Product { get; set; }
+        public int Count { get; set; }
+    }
+}
