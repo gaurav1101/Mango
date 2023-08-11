a394881ee7565d0e97b264fe4a764e9180310f83
Gaurav Kaushik <Gaurav.Kaushik@kelltontech.com>
2023-08-11 12:04:18 +0530

    migration scripts

--- Mango.Services.ShoppingCart/Program.cs
+++ Mango.Services.ShoppingCart/Program.cs
@@ -94,20 +94,3 @@ app.MapControllers();
 
 app.Run();
 
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
